24186dd33d45ac92fbe7672c913d1132caea2388
zephyrusg <bas.dekoning@gmail.com>
2025-07-25 11:22:27 +0200

    new stuff

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -52,5 +52,34 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.props["src"], "http://example.com/image.png")
         self.assertEqual(html_node.props["alt"], "Image")
+
+    def test_split_nodes_delimiter(self):
+        nodes = [TextNode("This is text with a **bolded phrase** in the middle", TextType.TEXT)]
+        delimiter = "**"
+        text_type = TextType.BOLD
+        new_nodes = split_nodes_delimiter(nodes, delimiter, text_type)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "bolded phrase")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " in the middle")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+        # Test with no delimiter
+        nodes = [TextNode("Hello world", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, delimiter, text_type)
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "Hello world")
+
+        # Test unmatched delimiter raises Exception
+        nodes = [TextNode("Hello **bla world", TextType.TEXT)]
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter(nodes, delimiter, TextType.BOLD)
+        self.assertEqual(
+            str(context.exception),
+            "Invalid Markdown syntax: unmatched delimiter '**' in text 'Hello **bla world'"
+        )
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -2,6 +2,7 @@ from enum import Enum
 from src.htmlnode import LeafNode
 
 class TextType(Enum):
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     PLAIN = "plain"
@@ -40,3 +41,31 @@ def text_node_to_html_node(text_node):
         return LeafNode(value="", tag="img", props={"src": text_node.url, "alt": text_node.text})
     else:
         raise ValueError(f"Unknown text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        # Only split nodes of type TEXT
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+        # If delimiter not found, just add the node
+        if len(parts) == 1:
+            new_nodes.append(node)
+            continue
+
+        # If odd number of delimiters, raise an error
+        if len(parts) % 2 == 0:
+            raise Exception(f"Invalid Markdown syntax: unmatched delimiter '{delimiter}' in text '{node.text}'")
+
+        # Alternate between TEXT and the given text_type
+        for i, part in enumerate(parts):
+            if part == "":
+                continue  # skip empty segments
+            if i % 2 == 0:
+                new_nodes.append(TextNode(part, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(part, text_type))
+    return new_nodes
